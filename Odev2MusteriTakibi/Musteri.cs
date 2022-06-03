59cbc34c00ccc902078a05f55854b14e4907c24e
Muhammet Taha Adıyaman <68828015+MuhammetTahaAdiyaman@users.noreply.github.com>
2022-06-03 17:52:45 +0300

    Create Musteri.cs

---
+++ Odev2MusteriTakibi/Musteri.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev2MusteriTakibi
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public string  Mail { get; set; }
+    }
+}
